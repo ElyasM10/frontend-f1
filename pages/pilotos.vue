11bce46a1a4eab33d790786a9a08bf52cff9237b
ElyasM10 <eliasmaldonado445@gmail.com>
2025-04-16 10:12:22 -0300

    Creacion inicial del front

---
+++ pages/pilotos.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data: pilotos, error } = await useFetch('http://localhost:8080/pilotos')
+</script>
+
+<template>
+  <div>
+    <h2>Pilotos</h2>
+    <ul>
+      <li v-for="piloto in pilotos" :key="piloto.id">
+        {{ piloto.nombre }} {{ piloto.apellido }}
+      </li>
+    </ul>
+  </div>
+</template
